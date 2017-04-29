regalstreak/android_vendor_oneplus_oneplus2

oneplus2: Initialise the oxygenos3 camera party

Android.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright 2016 halogenOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +18,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),oneplus2)
+
+# Call the OnePlus Camera makefile (We want merges to be easy AF)
+include vendor/oneplus/oneplus2/OxygenOSCamera.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := CNEService
@@ -302,6 +306,7 @@ LOCAL_MODULE_PATH_32 := $(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MULTILIB := both
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
+
 
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)

DesolationRom-Devices/android_vendor_oneplus_onyx

Change-Id: I86ebe4fa70d5a74af79d089e7d285d9bf0e867b8
Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016-2017 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +18,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),onyx)
+
+# Call the OnePlus Camera makefile (We want merges to be easy AF)
+include vendor/oneplus/onyx/OxygenOSCamera.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02